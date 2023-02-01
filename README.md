9685d331b0cbf6cd0dc1c7ce352966e6fe49cba4
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-01 11:20:10 +0000

    devex: set up dev command

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # vscode-csv-editor
 
 CSV editor for VSCode that works anywhere.
+
+## Development
+
+Development requires the following prerequisites:
+
+- [ ] Install [Node.js](https://nodejs.org/en/download/)
+- [ ] Install [pnpm](https://pnpm.io/installation)
+- [ ] Install dependencies with `pnpm install`
+
+Once you have the prerequisites installed, you can run the following commands for developing:
+
+```shell
+pnpm run dev
+```
--- packages/webui/src/App.tsx
+++ packages/webui/src/App.tsx
@@ -4,12 +4,15 @@ import { vscode } from './utilities/VSCodeAPIWrapper'
 import './App.css'
 
 export const App: React.FC = () => {
-  const [csvFile, setCSVFile] = useState<string[][]>([])
+  // TODO Add a loading state
+  const [csvFile, setCSVFile] = useState<string[][]>([['hello'], ['test']])
 
+  // updateData is a callback function that is passed to the TableComponent that sends data back to the extension.
   function updateData(data: string[][]) {
     vscode.postMessage({ command: 'update', data })
   }
 
+  // This useEffect is used to register listeners for messages from the extension.
   useEffect(() => {
     const handleMessage = (event: MessageEvent) => {
       console.log(event)
